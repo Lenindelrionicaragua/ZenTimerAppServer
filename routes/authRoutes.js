b40e2c94fab9dfe2fa297ad8abb8a09cc138eed3
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-01 16:40:36 +0200

    add routes to verifyEmail and resendVerificationLink

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,10 @@ import { signup } from "../controllers/authControllers/signupController.js";
 import { signInWithGoogleController } from "../controllers/authControllers/signInWithGoogleController.js";
 import { login } from "../controllers/authControllers/loginController.js";
 import { logout } from "../controllers/authControllers/logoutController.js";
+import {
+  resendVerificationLink,
+  verifyEmail,
+} from "../controllers/authControllers/emailVerificationController.js";
 
 const authRouter = express.Router();
 
@@ -10,5 +14,8 @@ authRouter.post("/sign-up", signup);
 authRouter.post("/sign-in-with-google", signInWithGoogleController);
 authRouter.post("/log-in", login);
 authRouter.post("/log-out", logout);
+
+authRouter.post("/resend-verification-link", resendVerificationLink);
+authRouter.get("/verify/:userId/:uniqueString", verifyEmail);
 
 export default authRouter;
