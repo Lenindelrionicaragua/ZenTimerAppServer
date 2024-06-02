5c1ab4a4d3202fa3aae825349a6e17fd6eb5942c
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-02 15:51:06 +0200

    in the file connectDB, set strictQuery to  true

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASSWORD,
+    pass: process.env.AUTH_PASSWORD.replace(/\s+/g, ""),
   },
 });
 
