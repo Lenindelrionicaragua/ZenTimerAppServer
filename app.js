2a667a8da39e90a51b361956bf9798c22a9ecc69
lenin del rio <lenindelrionicaragua@gmail.com>
2024-06-04 14:40:06 +0200

    add middleware to  focer HTTPS request in produccion evioroment

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { requireAuth } from "./middleware/authMiddleware.js";
 import userRouter from "./routes/userRoutes.js";
 import authRouter from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // Create an express server
 const app = express();
 
+// Middleware to force HTTPS
+app.use((req, res, next) => {
+  if (
+    req.headers["x-forwarded-proto"] !== "https" &&
+    process.env.NODE_ENV === "production"
+  ) {
+    return res.redirect("https://" + req.headers.host + req.url);
+  }
+  next();
+});
+
 // Tell express to use the json middleware
 app.use(express.json());
+
 // Restrict access to only our UI
 app.use(
   cors({
@@ -22,6 +34,18 @@ app.use(
     // origin: process.env.UI_BASE_URL,
   })
 );
+
+// Configure CORS based on the environment
+const corsOptions = {
+  credentials: true,
+  origin:
+    process.env.NODE_ENV === "production"
+      ? process.env.UI_BASE_URL
+      : "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
 app.use(cookieParser());
 
 /****** Attach routes ******/
