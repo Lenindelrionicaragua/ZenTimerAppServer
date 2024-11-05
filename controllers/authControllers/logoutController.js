b7aab63931a43dae94a1f5165b25aef2aebb01f4
lenin del rio <lenindelrionicaragua@gmail.com>
2024-11-05 13:11:44 +0100

    Enhance logout function: clear all relevant cookies, check for active session, and improve error handling.

--- controllers/authControllers/logoutController.js
+++ controllers/authControllers/logoutController.js
@@ -3,16 +3,31 @@ import { logInfo, logError } from "../../util/logging.js";
 export const logout = (req, res) => {
   try {
     console.log("Current cookies:", req.cookies);
-    res.clearCookie("session");
-    logInfo("User successfully logged out");
 
-    res
-      .status(200)
-      .json({ success: true, message: "User successfully logged out" });
+    // Check for active session
+    if (!req.cookies.session) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "No active session to log out from.",
+        });
+    }
+
+    // Clear cookies
+    res.clearCookie("session", { httpOnly: true, secure: true });
+    res.clearCookie("zenTimerToken", { httpOnly: true, secure: true }); // Add other relevant cookies
+
+    logInfo(`User with ID ${req.user.id} successfully logged out.`);
+
+    res.status(204).send(); // No content response
   } catch (error) {
-    logError(error);
+    logError("Logout error: ", error);
     res
       .status(500)
-      .json({ success: false, message: "An error occurred during logout" });
+      .json({
+        success: false,
+        message: error.message || "An error occurred during logout",
+      });
   }
 };
