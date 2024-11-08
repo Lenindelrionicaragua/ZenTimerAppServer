888f8b9514c676f19086011d261660b0ae446e2a
lenin del rio <lenindelrionicaragua@gmail.com>
2024-11-08 13:46:14 +0100

    create autoCreateDefaultCategoriesController.js to handle when de user does not have de default habit categories

---
+++ controllers/habitCategoriesControllers/autoCreateDefaultCategories.js
@@ -0,0 +1,19 @@
+import { autoCreateDefaultCategories } from "../../util/autoCreateDefaultCategories";
+import { logError } from "../../util/logging";
+
+export const autoCreateCategoriesController = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+    await autoCreateDefaultCategories(userId);
+
+    return res.status(200).json({ message: "Categories created successfully" });
+  } catch (error) {
+    logError("Error in autoCreateCategoriesController:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
