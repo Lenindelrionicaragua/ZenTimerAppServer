afb16fd8962a83b466e04a08fa09c28f664fd23b
lenin del rio <lenindelrionicaragua@gmail.com>
2024-11-18 16:26:20 +0100

    Simplified categoryId validation to check only for 24-character length

--- controllers/dailyTimeRecords/createDailyTimeRecords.js
+++ controllers/dailyTimeRecords/createDailyTimeRecords.js
@@ -8,10 +8,6 @@ export const createDailyTimeRecords = async (req, res) => {
   const { categoryId } = req.params;
   const userId = req.userId;
 
-  // logInfo(
-  //   `Received request to create/update daily record: userId=${userId}, categoryId=${categoryId}, minutesUpdate=${minutesUpdate}, date=${date}`
-  // );
-
   // Validate input
   const errorList = validateDailyRecords({
     userId,
@@ -30,10 +26,6 @@ export const createDailyTimeRecords = async (req, res) => {
   }
 
   try {
-    // logInfo(
-    //   `Validation passed for user ${userId} and category ${categoryId}. Checking for existing record.`
-    // );
-
     // Check for an existing record with the normalized date
     const existingRecord = await DailyTimeRecord.findOne({
       userId,
@@ -42,21 +34,11 @@ export const createDailyTimeRecords = async (req, res) => {
     });
 
     if (existingRecord) {
-      // logInfo(
-      //   `Existing record found for user ${userId} and category ${categoryId}. Updating the record.`
-      // );
       existingRecord.totalDailyMinutes += minutesUpdate; // Add minutes to existing record
       await existingRecord.save();
 
-      // logInfo(
-      //   `Updated daily record for user ${userId} and category ${categoryId}. Total minutes: ${existingRecord.totalDailyMinutes}`
-      // );
       return res.status(200).json({ success: true, record: existingRecord });
     }
-
-    // logInfo(
-    //   `No existing record found for user ${userId} and category ${categoryId}. Creating a new record.`
-    // );
 
     // If no existing record, create a new one
     const newRecord = new DailyTimeRecord({
@@ -68,9 +50,6 @@ export const createDailyTimeRecords = async (req, res) => {
 
     await newRecord.save();
 
-    // logInfo(
-    //   `Created new daily record for user ${userId} and category ${categoryId}. Total minutes: ${newRecord.totalDailyMinutes}`
-    // );
     return res.status(201).json({ success: true, record: newRecord });
   } catch (error) {
     logError("Error creating or updating daily record: ", error);
