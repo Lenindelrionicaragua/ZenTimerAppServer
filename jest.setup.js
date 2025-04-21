2cdbe7438763958c22bfd210ef6bac374abf73a8
lenin del rio <lenindelrionicaragua@gmail.com>
2025-04-21 12:06:01 +0200

    chore(test): run tests in series in CI and increase timeout globally

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 import { logInfo } from "./util/logging";
 dotenv.config({ path: ".env.test" });
 
-jest.setTimeout(60000);
+jest.setTimeout(100000);
 
 logInfo("Loaded RESEND_API_KEY:", process.env.RESEND_API_KEY);
